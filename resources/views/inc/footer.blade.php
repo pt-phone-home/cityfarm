503a0e45097b6491bb787902e585db3b870c8c21
Peter Tiernan <ptiernan@gmail.com>
2019-05-16 13:07:32 +0100

    updates

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,5 +1,57 @@
-<footer class="w-full bg-blue-darkest">
-    <div class="container w-full mx-auto h-32 py-4 text-grey-lightest">
-        Testing
+<footer class="w-full bg-primary-8">
+    <div class="container mx-auto py-4 flex flex-wrap text-grey-lightest">
+        <div class="w-full md:w-1/3 px-4 py-4">
+            <div class="flex flex-col">
+                <h3 class="text-center py-2 font-dec text-3xl text-grey-light">Links</h3>
+                <div class="flex justify-around">
+                    <div class="flex flex-col flex-grow">
+                        <h4 class="font-dec text-xl text-grey-light">Our Community</h4>
+                        <div class="flex flex-col flex-1 py-2">
+                            <a href="{{route('index')}}" class="no-underline text-grey hover:text-grey-light">Home</a>
+                            <a href="{{route('about')}}" class="no-underline text-grey hover:text-grey-light">About</a>
+                            <a href="{{route('classes')}}" class="no-underline text-grey hover:text-grey-light">Classes</a>
+                            <a href="{{route('calendar')}}" class="no-underline text-grey hover:text-grey-light">Calendar</a>
+                            <a href="{{route('news')}}" class="no-underline text-grey hover:text-grey-light">News</a>
+                        </div>
+                        <hr class="border border-grey w-full">
+                    </div>
+                    <div class="flex flex-col flex-grow ">
+                        <h4 class="font-dec text-xl text-grey-light">Love</h4>
+                        <div class="flex flex-col flex-1 py-2">
+                            <a href="{{route('animals')}}" class="no-underline text-grey hover:text-grey-light">Animals</a>
+                            <a href="{{route('grow')}}" class="no-underline text-grey hover:text-grey-light">Grow</a>
+                            <a href="{{route('education')}}" class="no-underline text-grey hover:text-grey-light">Education</a>
+                            <a href="{{route('ecology')}}" class="no-underline text-grey hover:text-grey-light">Ecology</a>
+                        </div>
+                        <hr class="border border-grey w-full">
+                    </div>
+                </div>
+                <div class="flex justify-around">
+                    <div class="flex flex-col flex-grow">
+                        <h4 class="font-dec text-xl text-grey-light">Get Involved</h4>
+                        <div class="flex flex-col flex-1 py-2">
+                            <a href="{{route('volunteer')}}" class="no-underline text-grey hover:text-grey-light">Volunteer</a>
+                            <a href="{{route('corporate')}}" class="no-underline text-grey hover:text-grey-light">Corporate</a>
+                            <a href="{{route('sponsors')}}" class="no-underline text-grey hover:text-grey-light">Sponsors</a>
+                            <a href="{{route('donate')}}" class="no-underline text-grey hover:text-grey-light">Donate</a>
+                        </div>
+                        <hr class="border border-grey w-full">
+                    </div>
+                    <div class="flex flex-col flex-grow">
+                        <h4 class="font-dec text-xl text-grey-light">Info</h4>
+                        <div class="flex flex-col flex-1 py-2">
+                            <a href="{{route('info.visit_us')}}" class="no-underline text-grey hover:text-grey-light">Visit Us</a>
+                            <a href="{{route('info.contact')}}" class="no-underline text-grey hover:text-grey-light">Contact</a>
+                            <a href="{{route('info.safety')}}" class="no-underline text-grey hover:text-grey-light">Safety</a>
+                        </div>
+                        <hr class="border border-grey w-full">
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        
+        
     </div>
 </footer>
