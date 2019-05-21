bded604cd2e79feed861da38439a950ebcedb369
Peter Tiernan <ptiernan@gmail.com>
2019-05-21 20:21:44 +0100

    login details updated

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,5 +12,10 @@ class UserSeeder extends Seeder
     public function run()
     {
         factory(App\User::class, 20)->create();
+        DB::table('users')->insert([
+            'name' => 'Peter Tiernan',
+            'email' => 'ptiernan@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
