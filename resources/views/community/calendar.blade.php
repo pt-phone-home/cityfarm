9b70c2f0de85084cbb641d564fc1e2a35cd892a8
Peter Tiernan <ptiernan@gmail.com>
2019-06-17 22:59:41 +0100

    minor updates to calendar page following launch

--- resources/views/community/calendar.blade.php
+++ resources/views/community/calendar.blade.php
@@ -65,7 +65,7 @@
 
         {{-- @if($event->date > Carbon::now()) --}}
 
-        <div class="w-full md:w-1/2 lg:w-1/3 flex justify-center items-center px-2 py-2 h-64">
+        <div class="w-full md:w-1/2 lg:w-1/3 flex justify-center items-center px-1 py-1 h-64">
             <div class="relative w-full h-full flex flex-col">
                 {{-- Backround Image and Overlay START --}}
                 <div class=" w-full h-full absolute pin-t pin-l z-0">
@@ -90,7 +90,7 @@
                 {{-- Top Section END --}}
                 {{-- Bottom Section START --}}
                 <div class="z-20 h-40p flex">
-                    <div class="w-70p bg-grey-light h-full px-2 py-2">
+                    <div class="w-70p bg-grey-light h-full text-sm px-1 py-2">
                         <h3 class="text-grey-darkest py-1">{{$event->title}}</h3>
                         <h4 class="py-1 text-grey-darkest">{{$event->location}}</h4>
                     <h5 class="text-grey-darkest">{{$event->time_from}} - {{$event->time_to}}</h5>
